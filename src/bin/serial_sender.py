eb2d5da110f5d8b9f5d59ca986aef1d1934d3005
VanderSant <vanderson.santos@thunderatz.org>
2021-08-26 13:25:14 -0300

    🚧  get a sucess test

--- src/bin/serial_sender.py
+++ src/bin/serial_sender.py
@@ -12,12 +12,19 @@
 """
 
 import serial
-while (True):
-    info = "00001"
-    ser = serial.Serial('/dev/ttyUSB0', 9600,timeout=1)
-    ser.write(info)
-    msg = ser.read()
-    print(msg)
+#init serial port and bound
+# bound rate on two ports must be the same
+#ser = serial.Serial(port='/dev/ttyUSB0', baudrate=9600, timeout=1)
+#print(ser.portstr)
+#
+##send data via serial port
+#ser.write('0')
+#ser.write('1')
+#ser.write('0')
+#ser.write('0')
+#ser.write('0')
+#ser.write('\n')
+#ser.close()
     
 #ser.write(b'5') #Prefixo b necessario se estiver utilizando Python 3.X 
 #with serial.Serial('/dev/ttyUSB0', 19200, timeout=1) as ser:
@@ -26,3 +33,32 @@ while (True):
 #    print(ser.name)         # check which port was really used
 #    ser.write(info)         # write a string
 #      ser.close()  
+
+import serial
+
+#Open a serial connection between Omega and ATmega
+new_port = serial.Serial(port='/dev/ttyUSB0', baudrate=9600, timeout=1)
+
+def read_data():
+    #read the output and store it in a variable
+    response = new_port.readline()
+    #Check to see if all the lines of the output are printed
+    while(len(response)>0):
+        print (response.rstrip())
+        response = new_port.readline()
+        
+
+def get_temperature(new_port):
+
+    read_data()
+    #check to see if the connection is open
+    if new_port.isOpen():
+            #while true, send 'r' char to receive the output from the sensor
+            new_port.write('10001\n')
+            #loop to print all the output lines
+
+#Function call
+get_temperature(new_port)
+
+#close the serial connection after the function is over
+new_port.close()
