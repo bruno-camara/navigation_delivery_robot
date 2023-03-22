e737c005c901a3e58dd5fd05a8248b2c343891cb
ZimaPoliUsp <zimausp@gmail.com>
2023-03-22 13:49:31 -0300

    🚧 new bash files to start autonomous navigation

---
+++ launch/complete_launches/bloco_c_jetson_launch.sh
@@ -0,0 +1,11 @@
+#! /home/Desktop/Zima/catkin_ws/
+catkin_make
+source devel/setup.bash
+
+sudo chmod 666 /dev/ttyUSB0
+sudo chmod 666 /dev/ttyUSB1
+
+
+roslaunch d_hospital_navigation bloco_c.launch &
+roslaunch d_hospital_avigation bloco_c_dysplay.launch &
+cd src/d_hospital_navigation/src/server_test/ && npm start &
