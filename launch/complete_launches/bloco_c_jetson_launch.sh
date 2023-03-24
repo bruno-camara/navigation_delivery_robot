b17f1325bba0afe24db7467fd9ca309fff3049fc
Pedro Croso <pedrocroso@usp.br>
2023-03-24 12:38:52 -0300

    🐛 minor bug

--- launch/complete_launches/bloco_c_jetson_launch.sh
+++ launch/complete_launches/bloco_c_jetson_launch.sh
@@ -9,6 +9,5 @@ sudo chmod 666 /dev/ttyUSB1
 
 
 roslaunch d_hospital_navigation bloco_c.launch &
-#roslaunch d_hospital_avigation bloco_c_dysplay.launch &
 rosrun d_hospital_navigation goal_from_app.py &
 cd src/d_hospital_navigation/src/server_test/ && npm start &
